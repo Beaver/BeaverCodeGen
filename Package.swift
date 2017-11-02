6a07fb57834939ddaee6e4f31b891993c63b1b26
Theo <theo@scribd.com>
2017-11-01 22:06:17 -0700

    Fixed package name

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "Commander",
+  name: "BeaverCodeGen",
   products: [
     .library(name: "BeaverCodeGen", targets: ["BeaverCodeGen"]),
   ],
